296fbee690254605fd10e3d7ba31809490033fc0
Brian A. Randell <brianr@mcwtech.com>
2020-10-27 16:01:48 -0700

    Created a new ASP.NET Web Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cinnug.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
